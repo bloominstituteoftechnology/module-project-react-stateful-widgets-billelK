53412c8d94df8309f278ae23fb14b2934d7caaa9
Billel Khansal <bilalkhansal@gmail.com>
2024-02-27 19:48:19 +0100

    Spinner component done

--- frontend/components/Spinner.js
+++ frontend/components/Spinner.js
@@ -36,23 +36,28 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Spinner() {
 /* STEP 1 */
-
+let [isVisible, toggleVisibility] = useState(false)
   const toggleSpinner = () => {
   /* STEP 4 */
+    if (isVisible === false) {
+      toggleVisibility(true)
+    } else {
+      toggleVisibility(false)
+    }
   };
 
   return (
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        isVisible && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+      {isVisible === false ? "Show Spinner" : "Hide Spinner"/* STEP 3 */}
       </button>
     </div>
   );
